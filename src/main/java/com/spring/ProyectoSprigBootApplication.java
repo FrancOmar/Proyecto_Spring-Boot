7dbc1f25c6c9aa04f000e87211a064eb02db21ce
Omar Franco Mollericona Sejas <omar.franco.7885@gmail.com>
2020-05-27 19:03:50 -0400

    Proyecto_Spring-Boot

---
+++ src/main/java/com/spring/ProyectoSprigBootApplication.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSprigBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSprigBootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/spring/repository/ContactRepository.java
@@ -0,0 +1,12 @@
+package com.spring.repository;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.spring.entity.Contact;
+
+@Repository("contactRepository")
+public interface ContactRepository extends JpaRepository<Contact, Serializable>{
+}
